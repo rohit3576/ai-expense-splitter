e7de27f000ee2a031fe9a0086196803cd0ad1da2
rohit3576 <rohit03576@gmail.com>
2026-03-28 23:29:16 +0530

    Initial Spring Boot setup with entities and MySQL configuration

---
+++ src/main/java/com/example/ai_expense_spliter/AiExpenseSpliterApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_expense_spliter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiExpenseSpliterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiExpenseSpliterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ai_expense_spliter/AiExpenseSpliterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_expense_spliter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiExpenseSpliterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
